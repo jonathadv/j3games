ccba9be743f54b00b6ba64e172ce2df65430b1ab
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2025-07-24 14:45:46 -0300

    feat: add embedded ruffle to run games

--- assets/j3games/main.js
+++ assets/j3games/main.js
@@ -33,93 +33,105 @@ const gameHelp = [
   },
 ];
 
+function getGamesList() {
+  var gameNames = [
+    "Alloy",
+    "ATV Extreme",
+    "Americas Army",
+    "Batman",
+    "Beer Monster",
+    "Beetle Buggin",
+    "Bike Mania",
+    "bilhar",
+    "Bomberman",
+    "bomb jack",
+    "Bowman",
+    "Cable Capers",
+    "Car Park Challenge",
+    "Crossing Cup",
+    "Dogfight - The Great War",
+    "Donkey Kong Bananas",
+    "Dragon Ball Z",
+    "Dress Up",
+    "Elite Forces",
+    "Galan, the Armadillo Knigth",
+    "Gmax Skate",
+    "Goku Paint",
+    "gold miner2",
+    "gold miner",
+    "Gyro Ball",
+    "helicopter",
+    "Hit The Jackpot",
+    "Hunga Hatch",
+    "IQ Marathon",
+    "Mario Jet",
+    "Metal Slug",
+    "Mini Golf",
+    "moon lander",
+    "N2O Rush",
+    "pacman",
+    "panik",
+    "penguin_baseball",
+    "pipes",
+    "polar rescue",
+    "Red Beard",
+    "rooftop skate",
+    "Scooby Doo Castelo Fantasma",
+    "Scooby Doo fugindo do Fantasma",
+    "Shackle-Man",
+    "Sheepish",
+    "Shove It",
+    "sonic",
+    "Steppenwolf",
+    "Street Fighter",
+    "Stunt Mania",
+    "Suckaz",
+    "super mario flash 2.0",
+    "supreme extreme snowboarding",
+    "surfs up",
+    "Through The Machine",
+    "Tube Racer",
+    "Tuning",
+    "uphill rush",
+    "vanguard mercenary",
+    "zombie-terror",
+  ];
+
+  return gameNames
+}
+
+function getGameUrl(gameName) {
+  return encodeURI(`jogos/${gameName}.swf`);
+}
+
+function getGameImage(gameName) {
+  return encodeURI(`jogos/${gameName}.gif`);
+}
+
 function loadGames() {
-  {
-    var gameNames = [
-      "Alloy",
-      "ATV Extreme",
-      "Americas Army",
-      "Batman",
-      "Beer Monster",
-      "Beetle Buggin",
-      "Bike Mania",
-      "bilhar",
-      "Bomberman",
-      "bomb jack",
-      "Bowman",
-      "Cable Capers",
-      "Car Park Challenge",
-      "Crossing Cup",
-      "Dogfight - The Great War",
-      "Donkey Kong Bananas",
-      "Dragon Ball Z",
-      "Dress Up",
-      "Elite Forces",
-      "Galan, the Armadillo Knigth",
-      "Gmax Skate",
-      "Goku Paint",
-      "gold miner2",
-      "gold miner",
-      "Gyro Ball",
-      "helicopter",
-      "Hit The Jackpot",
-      "Hunga Hatch",
-      "IQ Marathon",
-      "Mario Jet",
-      "Metal Slug",
-      "Mini Golf",
-      "moon lander",
-      "N2O Rush",
-      "pacman",
-      "panik",
-      "penguin_baseball",
-      "pipes",
-      "polar rescue",
-      "Red Beard",
-      "rooftop skate",
-      "Scooby Doo Castelo Fantasma",
-      "Scooby Doo fugindo do Fantasma",
-      "Shackle-Man",
-      "Sheepish",
-      "Shove It",
-      "sonic",
-      "Steppenwolf",
-      "Street Fighter",
-      "Stunt Mania",
-      "Suckaz",
-      "super mario flash 2.0",
-      "supreme extreme snowboarding",
-      "surfs up",
-      "Through The Machine",
-      "Tube Racer",
-      "Tuning",
-      "uphill rush",
-      "vanguard mercenary",
-      "zombie-terror",
-    ];
+  var gameNames = getGamesList()
 
+  var leftList = ['<div class="col-md-5"><div class="container">\n'];
+  var centerList = ['<div class="col-md-1"><div class="container"><p class="container"></p></div></div>\n'];
+  var rightList = ['<div class="col-md-5"><div class="container">\n'];
 
-    var leftList = ['<div class="col-md-5"><div class="container">\n'];
-    var centerList = ['<div class="col-md-1"><div class="container"><p class="container"></p></div></div>\n'];
-    var rightList = ['<div class="col-md-5"><div class="container">\n'];
+  for (var i = 0; i < gameNames.length; i++) {
+    var gameName = gameNames[i];
+    var imageSrc = getGameImage(gameName);
+    var swfLink = getGameUrl(gameName);
+    var htmlLine = `<p class="container"><img src="${imageSrc}"><a href="#" onclick=loadGameInRufflePlayer("${swfLink}")><span class="gameName">${gameName}</span></a></p>\n`;
 
-    for (var i = 0; i < gameNames.length; i++) {
-      var gameName = gameNames[i];
-      var imageSrc = `jogos/${gameName}.gif`;
-      var swfLink = `jogos/${gameName}.swf`;
-
-      if (i % 2 === 0) {
-        leftList.push(`<p class="container"><img src="${imageSrc}"><a href="${swfLink}"><span class="gameName">${gameName}</span></a></p>\n`);
-      } else {
-        rightList.push(`<p class="container"><img src="${imageSrc}"><a href="${swfLink}"><span class="gameName">${gameName}</span></a></p>\n`);
-      }
+    if (i % 2 === 0) {
+      leftList.push(htmlLine);
+    } else {
+      rightList.push(htmlLine);
     }
-    leftList.push("</div></div>\n")
-    rightList.push("</div></div>\n")
-    var html = leftList.concat(centerList).concat(rightList);
-
-    $('#gameList').append(html.join(""));
   }
+  leftList.push("</div></div>\n")
+  rightList.push("</div></div>\n")
+  var html = leftList.concat(centerList).concat(rightList);
+
+  $('#gameList').append(html.join(""));
 }
 
 function loadHeader() {
@@ -154,11 +166,11 @@ function loadBackButton() {
 function loadGameHelp() {
   var html = [];
   var gameNames = [];
-  
+
 
   gameHelp.forEach(item => {
     gameNames.push(`<span><a href="#${item.name}">${item.name}</a>, </span>\n`)
-    
+
     html.push("<div>\n");
     html.push(`
     <h5 name="${item.name}" id="${item.name}">
@@ -176,10 +188,36 @@ function loadGameHelp() {
   $('#gameHelp').append(gameNames.join("") + html.join(""));
 }
 
+function loadRufflePlayer() {
+  if (window.RufflePlayer) {
+    console.log("Loading RufflePlayer")
+    window.RufflePlayer = window.RufflePlayer || {};
+
+    const ruffle = window.RufflePlayer.newest();
+    const player = ruffle.createPlayer("");
+    const container = document.getElementById("gameContainer");
+    container.appendChild(player);
+    window.curentRufflePlayer = player;
+
+    var firstGameUrl = getGameUrl(getGamesList()[0]);
+    player.ruffle().load(firstGameUrl);
+  }
+  else {
+    console.log("Not loading RufflePlayer.")
+  }
+
+}
+
+function loadGameInRufflePlayer(gamePath) {
+  console.log("Loading game: " + gamePath)
+  window.curentRufflePlayer.ruffle().load(gamePath);
+}
+
 $(document).ready(function () {
   loadHeader()
   loadFooter()
   loadBackButton()
   loadGames()
   loadGameHelp()
+  loadRufflePlayer()
 });
